83037fc0b1d298e98536bd586246237c9184e741
TianxingWu <tianxingwu.ntu@gmail.com>
2025-04-21 07:26:25 +0800

    debug

--- tx_get_density_map.py
+++ tx_get_density_map.py
@@ -136,10 +136,11 @@ for sub_clip_path in tqdm(sub_clip_paths[:n_samples]):
 
             # Scale to [0, 255] and convert to uint8
             rho_uint8 = (rho_normalized * 255).astype(np.uint8)
+            rho_uint8_rgb = np.repeat(rho_uint8, 3, axis=-1)
 
             video_path = f'/mnt/Text2Video/fanweichen/tx/dataset/mflow/4DGen-Dataset-tx/Human_Raw_Data/pexels/{part1_segs[1]}/{part1_seg2_prefix}/{part1_seg2}.mp4'
             video_info = sv.VideoInfo.from_video_path(video_path)  # get video info
-            write_video(os.path.join(save_dir, 'density_map.mp4'), rho_uint8, video_info.fps)
+            write_video(os.path.join(save_dir, 'density_map.mp4'), rho_uint8_rgb, video_info.fps)
 
     except Exception as e:
         logging.info(f"FAILED: {sub_clip_path} ({e})")
